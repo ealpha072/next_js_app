67a07810522820030458c7bcabc0c2dcbedea585
ealpha072 <ealpha072@gmail.com>
2023-03-07 22:23:12 +0300

    Pricing page

---
+++ src/pages/routes/pricing.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head"
+
+export default function Pricing(){
+    return(
+        <>
+            <Head>
+                <title>Pricing - Alpha Site</title>
+                <meta name="description" content="Pricing details" />
+            </Head>
+            <div>
+                <div>
+                    <h1>Create better content. Faster</h1>
+                    <p>
+                        Create, manage and design anything in one place 
+                        Trusted by leading teams and organizations.
+                    </p>
+                </div>
+
+            </div>
+        </>
+    )
+}
