bdfa43df7e7399a66fc6354f7f56647fc89e975a
ealpha072 <ealpha072@gmail.com>
2023-03-07 22:31:20 +0300

    Pricing tag div

--- src/pages/routes/pricing.tsx
+++ src/pages/routes/pricing.tsx
@@ -14,8 +14,33 @@ export default function Pricing(){
                         Create, manage and design anything in one place 
                         Trusted by leading teams and organizations.
                     </p>
+                    <div>
+                        <button>Monthly</button>
+                        <button>Annually</button>
+                    </div>
                 </div>
 
+            </div>
+            <div>
+                <div>
+                    <h1>Basic</h1>
+                    <p>Use it at no cost</p>
+                    <p>Use it as long as you want</p>
+
+                    <div>
+                        <h1>Free</h1>
+                        <button>Get Started</button>
+                    </div>
+
+                    <div>
+                        <p>Access to:</p>
+                        <ul>
+                            <li>100 MB Storage</li>
+                            <li>Template Design Assets</li>
+                            <li>Most templates</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </>
     )
