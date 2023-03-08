ea00ba5760c088bb4135de13494547daa7154a51
ealpha072 <ealpha072@gmail.com>
2023-03-08 22:55:57 +0300

    Styling for pricing

--- src/pages/routes/pricing.tsx
+++ src/pages/routes/pricing.tsx
@@ -10,40 +10,80 @@ export default function Pricing(){
                 <meta name="description" content="Pricing details" />
             </Head>
             <Header />
-            <div className={styles.pricingContainer}>
-                <div>
-                    <h1>Create better content. Faster</h1>
-                    <p>
-                        Create, manage and design anything in one place 
-                        Trusted by leading teams and organizations.
-                    </p>
-                    <div className={styles.buttons}>
-                        <button>Monthly</button>
-                        <button>Annually</button>
+            <div className={styles.mainPricing}>
+                <div className={styles.pricingContainer}>
+                    <div>
+                        <h1>Create better content. Faster</h1>
+                        <p>
+                            Create, manage and design anything in one place 
+                            Trusted by leading teams and organizations.
+                        </p>
+                        <div className={styles.buttons}>
+                            <button>Monthly</button>
+                            <button>Annually</button>
+                        </div>
                     </div>
+
                 </div>
 
-            </div>
-            
-            <div>
-                <div>
-                    <h1>Basic</h1>
-                    <p>Use it at no cost</p>
-                    <p>Use it as long as you want</p>
+                <div className={styles.pricingCardCont}>
+                    <div className={styles.priceCard}>
+                        <h1>Basic</h1>
+                        <p>Use it at no cost <br /> Use it as long as you want </p>
 
-                    <div>
-                        <h1>Free</h1>
-                        <button>Get Started</button>
+                        <div>
+                            <h2>Free</h2>
+                            <button>Get Started</button>
+                        </div>
+
+                        <div>
+                            <h3>Access to:</h3>
+                            <ul>
+                                <li>100 MB Storage</li>
+                                <li>Template Design Assets</li>
+                                <li>Most templates</li>
+                            </ul>
+                        </div>
                     </div>
 
-                    <div>
-                        <p>Access to:</p>
-                        <ul>
-                            <li>100 MB Storage</li>
-                            <li>Template Design Assets</li>
-                            <li>Most templates</li>
-                        </ul>
+                    <div className={styles.priceCard}>
+                        <h1>Basic</h1>
+                        <p>Use it at no cost <br /> Use it as long as you want </p>
+
+                        <div>
+                            <h2>Free</h2>
+                            <button>Get Started</button>
+                        </div>
+
+                        <div>
+                            <h3>Access to:</h3>
+                            <ul>
+                                <li>100 MB Storage</li>
+                                <li>Template Design Assets</li>
+                                <li>Most templates</li>
+                            </ul>
+                        </div>
                     </div>
+
+                    <div className={styles.priceCard}>
+                        <h1>Basic</h1>
+                        <p>Use it at no cost <br /> Use it as long as you want </p>
+
+                        <div>
+                            <h2>Free</h2>
+                            <button>Get Started</button>
+                        </div>
+
+                        <div>
+                            <h3>Access to:</h3>
+                            <ul>
+                                <li>100 MB Storage</li>
+                                <li>Template Design Assets</li>
+                                <li>Most templates</li>
+                            </ul>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </>
